df7627d5f7dfa36e238e9d5782c17850426ca73a
Luis Mario <itsluismario@gmail.com>
2023-02-14 15:10:23 -0600

    :sparkles: Numbers in typescript

---
+++ src/04-numbers.ts
@@ -0,0 +1,32 @@
+(() => {
+  let productPrice = 100;
+  productPrice =12;
+  console.log('productoPrice', productPrice);
+
+  let customerAge: number= 28;
+  customerAge = customerAge + 1;
+  console.log('customerAge',  customerAge);
+
+  let produdctInStock: number;
+  console.log('productInStock', produdctInStock);
+if (produdctInStock > 10) {
+  console.log('produdctInStock', produdctInStock);
+}
+
+let discount = parseInt('123');
+console.log('discount',discount);
+if(discount <= 200) {
+  console.log('apply');
+} else {
+  console.log('not apply');
+}
+
+  let hex = 0xfff;
+  console.log('hex',hex);
+
+  let bin = 0b1010;
+  console.log('bin',bin);
+
+  const myNumber: number = 10;
+
+})();
