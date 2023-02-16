7057ae6a3ef7c7568f2108737c67edbc78775b0e
Luis Mario <itsluismario@gmail.com>
2023-02-16 11:15:11 -0600

    :sparkles: Modules: import & export

---
+++ src/products/main.ts
@@ -0,0 +1,17 @@
+import { addProduct, calcStock, products } from "./product.service";
+
+addProduct({
+  title: 'P1',
+  createAt: new Date(1997, 7, 26),
+  stock: 12
+});
+
+addProduct({
+  title: 'P2',
+  createAt: new Date(1997, 11, 11),
+  stock: 6
+});
+
+console.log(products);
+const total = calcStock();
+console.log(total);
